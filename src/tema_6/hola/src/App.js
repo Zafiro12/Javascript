0612652992d53f5ddc356d208776f9ff31bc35c8
Zafiro12 <alexsanta1205@gmail.com>
2022-12-01 11:51:44 +0100

    hola

---
+++ src/tema_6/hola/src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+
+constructor(props){
+super(props);
+this.state = {
+saludo:"SALUDA EN VARIOS IDIOMAS",
+};
+this.mensajeingles = this.ingles.bind(this)
+}
+
+ingles(){ this.setState({saludo:"HELLO"}) }
+aleman(){ this.setState({saludo:"HALLO"}) }
+sueco(){ this.setState({saludo:"HEJ"}) }
+espanol(){ this.setState({saludo:"HOLA"}) }
+
+render() {
+//
+
+return(
+<>
+<button onClick={this.mensajeingles}>INGLES</button>
+<button onClick={()=>this.aleman()}>ALEMAN</button>
+<button onClick={()=>this.sueco()}>SUECO</button>
+<button onClick={()=>this.espanol()}>ESPAÑOL</button><br/>
+{this.state.saludo}
+</>
+);
+
+}
+}
+
+export default App;
