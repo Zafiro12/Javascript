e6cdc9c452d637c94559ddfa7c0a8a4e1b322aa9
Zafiro12 <alexsanta1205@gmail.com>
2022-12-15 10:56:10 +0100

    hecho colorines

---
+++ tema_6/colorines/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react"
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Button, Spinner } from 'reactstrap'
+
+function Pulso(props) {
+  return (
+    <Spinner type='grow' color={props.color} />
+  )
+}
+
+function Botones(props) {
+  const coloresDefault = [{ nombre: 'Azul', color: 'primary' }, { nombre: 'Rojo', color: 'danger' }, { nombre: 'Verde', color: 'success' }, { nombre: 'Amarillo', color: 'warning' }]
+
+  const colores = props.colores || coloresDefault
+  const [color, setColor] = useState('primary')
+
+  const handleClick = (e) => {
+    setColor(e)
+    console.log(e)
+  }
+
+  return (
+    <>
+      {colores.map((color, i) => <Button onClick={() => handleClick(color.color)} key={i} color={color.color} outline>{color.nombre}</Button>)}
+      <br />
+      <Pulso color={color} />
+    </>
+  )
+}
+
+function App() {
+  const colores = [{ nombre: 'Azul', color: 'primary' }, { nombre: 'Rojo', color: 'danger' }, { nombre: 'Verde', color: 'success' }, { nombre: 'Amarillo', color: 'warning' }, { nombre: 'Gris', color: 'secondary' }, { nombre: 'Negro', color: 'dark' }]
+  return (
+    <>
+      <Botones colores={colores} />
+    </>
+  )
+}
+
+export default App
