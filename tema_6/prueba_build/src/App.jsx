3606889716d9b62b6863054e0a6374cbcd3e8f3d
Zafiro12 <alexsanta1205@gmail.com>
2022-12-21 12:33:26 +0100

    Hecho build

--- tema_6/prueba_build/src/App.jsx
+++ tema_6/prueba_build/src/App.jsx
@@ -7,7 +7,7 @@ function App(props) {
     <div className="seccion-tarjetas">
       {
         Array(props.cantidad).fill().map((_, i) => (
-            <Tarjeta key={i} titulo="Titulo" src="src/assets/imagen1.jpg" parrafo="Parrafo" />
+            <Tarjeta key={i} titulo="Titulo" src="imagen1.jpg" parrafo="Parrafo" />
         ))
       }
     </div>
