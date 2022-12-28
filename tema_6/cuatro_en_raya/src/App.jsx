243afbbb789e92c5120f6ac0a3e9a9cd39be7dcd
Zafiro12 <alexsanta1205@gmail.com>
2022-12-28 14:14:43 +0100

    funcional

--- tema_6/cuatro_en_raya/src/App.jsx
+++ tema_6/cuatro_en_raya/src/App.jsx
@@ -6,44 +6,42 @@ const MapaBotones = (props) => {
   let listaBotones = props.listaBotones;
   return (
     <div>
-      {listaBotones.map((fila, x) => {
-        return (
-          <div key={x}>
-            {
-            fila.map((boton, y) => {
-              if (x === 0 && boton !== "secondary") 
+      {
+        listaBotones.map((fila, x) => {
+          return (
+            <div key={x}>
               {
-                return (
-                  <Button
-                    key={y}
-                    onClick={() => props.onClick(x, y)}
-                    color={boton}
-                  ></Button>
-                );
+                fila.map((boton, y) => {
+                  if (x === 0 && boton !== "secondary") {
+                    return (
+                      <Button
+                        key={y}
+                        onClick={() => props.onClick(x, y)}
+                        color={boton}
+                      ></Button>
+                    );
+                  }
+                  if (x === 0) {
+                    return (
+                      <Button
+                        key={y}
+                        onClick={() => props.onClick(x, y)}
+                        color={boton}
+                        outline
+                      ></Button>
+                    );
+                  }
+                  if (boton !== "secondary") {
+                    return <Button key={y} color={boton}></Button>;
+                  } else {
+                    return <Button key={y} color={boton} disabled outline></Button>;
+                  }
+                })
               }
-              if (x === 0) 
-              {
-                return (
-                  <Button
-                    key={y}
-                    onClick={() => props.onClick(x, y)}
-                    color={boton}
-                    outline
-                  ></Button>
-                );
-              }
-              if (boton !== "secondary") 
-              {
-                return <Button key={y} color={boton}></Button>;
-              } else 
-              {
-                return <Button key={y} color={boton} disabled outline></Button>;
-              }
-            })
-            }
-          </div>
-        );
-      })}
+            </div>
+          );
+        })
+      }
     </div>
   );
 };
@@ -53,37 +51,114 @@ class App extends Component {
     super(props);
     this.state = {
       listaBotones: Array(9).fill(null),
-      turno: false
+      turno: false,
+      ganaAmarillo: false,
+      ganaRojo: false,
     };
   }
 
-  clica(x, y) {
-    console.log(x, y);
-
-    let listaBotones = [...this.state.listaBotones];
-    
-    for (let i = listaBotones.length - 1; i >= 0; i--) {
-      if (listaBotones[i][y] !== "primary") {
-        listaBotones[i][y] = "primary";
-        break;
-      }
-    }
-
-    this.setState({ listaBotones: listaBotones });
-  }
-
-  componentWillMount() {
+  reset() {
     let listaBotones = [...this.state.listaBotones];
     listaBotones = listaBotones.map((fila) => {
       return Array(9).fill("secondary");
     });
     this.setState({ listaBotones: listaBotones });
+    this.setState({ turno: false });
+    this.setState({ ganaAmarillo: false });
+    this.setState({ ganaRojo: false });
+  }
+
+  buscarGanador() {
+    let listaBotones = [...this.state.listaBotones];
+
+    // Comprobar filas
+    listaBotones.forEach((fila) => {
+      let contadorAmarillo = 0;
+      let contadorRojo = 0;
+      fila.forEach((boton) => {
+        if (boton === "warning") {
+          contadorAmarillo++;
+          contadorRojo = 0;
+        } else if (boton === "danger") {
+          contadorRojo++;
+          contadorAmarillo = 0;
+        } else {
+          contadorAmarillo = 0;
+          contadorRojo = 0;
+        }
+        if (contadorAmarillo === 4) {
+          this.setState({ ganaAmarillo: true });
+          return;
+        }
+        if (contadorRojo === 4) {
+          this.setState({ ganaRojo: true });
+        }
+      });
+    });
+
+    // Comprobar columnas
+    for (let i = 0; i < listaBotones.length; i++) {
+      let contadorAmarillo = 0;
+      let contadorRojo = 0;
+      for (let j = 0; j < listaBotones.length; j++) {
+        if (listaBotones[j][i] === "warning") {
+          contadorAmarillo++;
+          contadorRojo = 0;
+        } else if (listaBotones[j][i] === "danger") {
+          contadorRojo++;
+          contadorAmarillo = 0;
+        } else {
+          contadorAmarillo = 0;
+          contadorRojo = 0;
+        }
+        if (contadorAmarillo === 4) {
+          this.setState({ ganaAmarillo: true });
+          return;
+        }
+        if (contadorRojo === 4) {
+          this.setState({ ganaRojo: true });
+          return;
+        }
+      }
+    }
+  }
+
+  clica(x, y) {
+    if (this.state.ganaAmarillo || this.state.ganaRojo) {
+      this.reset();
+      return;
+    }
+
+    let listaBotones = [...this.state.listaBotones];
+
+    for (let i = listaBotones.length - 1; i >= 0; i--) {
+      if (listaBotones[i][y] !== "danger" && listaBotones[i][y] !== "warning") {
+        if (this.state.turno) {
+          listaBotones[i][y] = "danger";
+        } else {
+          listaBotones[i][y] = "warning";
+        }
+        break;
+      }
+    }
+
+    this.setState({ turno: !this.state.turno });
+    this.setState({ listaBotones: listaBotones });
+
+    this.buscarGanador();
+  }
+
+  componentWillMount() {
+    this.reset();
   }
 
   render() {
     return (
       <div className="App">
         <h1> CUATRO EN RAYA </h1>
+        <h2> Turno: {this.state.turno ? "Rojo" : "Amarillo"} </h2>
+        <h2> {this.state.ganaAmarillo ? "Gana Amarillo" : ""} </h2>
+        <h2> {this.state.ganaRojo ? "Gana Rojo" : ""} </h2>
         <MapaBotones
           listaBotones={this.state.listaBotones}
           onClick={(x, y) => this.clica(x, y)}
