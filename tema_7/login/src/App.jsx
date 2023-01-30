3c64b67579ec1d1119859017adba11fe3cc9f796
Zafiro12 <alexsanta1205@gmail.com>
2023-01-30 09:47:01 +0100

    fix

--- tema_7/login/src/App.jsx
+++ tema_7/login/src/App.jsx
@@ -8,20 +8,26 @@ import {PHPLOGIN} from "./data.js";
 function App() {
     const [activa, setActiva] = useState(false);
     const [correcto, setCorrecto] = useState(true);
+    const [error, setError] = useState(false);
 
     function comprobar(usuario, clave) {
         axios.post(PHPLOGIN, JSON.stringify({usuario: usuario, clave: md5(clave)})).then(r => {
             if (r.data.mensaje === "aprobado") {
                 setActiva(true);
                 setCorrecto(true);
+                setError(false);
             }
             if (r.data.mensaje === "denegado") {
                 setCorrecto(false);
+                setError(false);
+            }
+            else {
+                setError(true)
             }
         });
     }
 
-    const obj = activa ? <Menu/> : <Login correcto={correcto} f={comprobar}/>
+    const obj = activa ? <Menu/> : <Login error={error} correcto={correcto} f={comprobar}/>
     return (<>
             {obj}
         </>)
--- tema_7/login/src/components/Login.jsx
+++ tema_7/login/src/components/Login.jsx
@@ -9,7 +9,8 @@ function Login(props) {
         props.f(usuario, clave);
     }
 
-    const obj = props.correcto ? <></> : <span style={{color: "red"}}>Usuario o contraseña no válidos</span>
+    const correcto = props.correcto ? <></> : <span style={{color: "red"}}>Usuario o contraseña no válidos</span>
+    const error = props.error ? <></> : <span style={{color: "red"}}>Ha ocurrido un error</span>
 
     return (<div style={{width: "100vw", height:"100vh"}} className="d-flex justify-content-center align-items-center">
         <Card
@@ -18,21 +19,22 @@ function Login(props) {
             style={{width: "18rem", flex: "0 18rem"}}
         >
             <CardTitle tag="h5">Iniciar sesión</CardTitle>
-            {obj}
+            {correcto}
+            {error}
             <CardText>
                 <Form>
                     <FormGroup>
-                        <Input type="text" placeholder="Nombre de usuario"
+                        <Input type="text" placeholder="Nombre de usuario" disabled={props.error}
                                onChange={event => setUsuario(event.target.value)}/>
                     </FormGroup>
 
                     <FormGroup>
-                        <Input type="password" placeholder="Contraseña"
+                        <Input type="password" placeholder="Contraseña" disabled={props.error}
                                onChange={event => setClave(event.target.value)}/>
                     </FormGroup>
                 </Form>
             </CardText>
-            <Button onClick={handleClick}>Iniciar sesión</Button>
+            <Button onClick={handleClick} disabled={props.error}>Iniciar sesión</Button>
         </Card>
     </div>);
 }
